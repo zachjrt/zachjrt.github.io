b6adb0626883de931f13220c7e93973235614607
zachjrt <tho19091@byui.edu>
2021-01-11 18:34:52 -0700

    Commit Revision

---
+++ main.js
@@ -0,0 +1,6 @@
+
+  var date = document.lastModified;
+  document.write("Last Modified: " + date);
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById("year").innerHTML = n;
