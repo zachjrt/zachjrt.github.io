f5ec812b68de388e66ab3f9c764298b162dcee01
zachjrt <tho19091@byui.edu>
2021-01-20 19:30:56 -0700

    commit

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
- 
-  var d = new Date();
-  var n = d.getFullYear();
-  document.getElementById("year").innerHTML = n;
-  document.getElementById("modified") = "Last Modified: " + d;
+ const thisYear = new Date();
+ let year = thisYear.getFullYear();
+ document.getElementById("year").textContent = year;
+
+ const lastModDate = new Date(document.lastModified);
+let strLMD = + lastModDate.getMonth()+1 + "/" + lastModDate.getDate() + "/" 
+           + lastModDate.getFullYear() + " " + lastModDate.getHours() + ":" 
+           + lastModDate.getMinutes() + ":" + lastModDate.getSeconds();
+document.getElementById("modified").textContent = strLMD;
