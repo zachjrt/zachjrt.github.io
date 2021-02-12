7c07e2796c12da3bfb704b8d60abff2e98c1a1c8
zachjrt <tho19091@byui.edu>
2021-02-11 18:55:18 -0700

    commit

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,15 @@
+const temperature = document.getElementById('temp').innerHTML;
+const windspeed = document.getElementById('speed').innerHTML;
+
+
+let windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temperature * Math.pow(windspeed, .16));
+  if (temperature <= 50 && windspeed > 3) {
+     windchill = Math.round(windchill);
+  } else {
+     windchill = "n/a";
+  }
+document.getElementById('chill').innerHTML = windchill;
+
+const thisYear = new Date();
+let year = thisYear.getFullYear();
+document.getElementById("year").textContent = year;
