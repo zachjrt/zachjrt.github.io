bdd327f50826467ea7718600cf2d8b6ad3ae91c8
zachjrt <tho19091@byui.edu>
2021-02-16 19:27:08 -0700

    commit

---
+++ lesson-7/main.js
@@ -0,0 +1,33 @@
+const images = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+}
+
+
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 300px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, 
+    imgObserver) =>{
+            entries.foeEach(entry =>{
+                if (!entry.isIntersecting) {
+                    return;
+                } else {
+                    preloadImage(entry.target);
+                    imgObserver.unobserve(entry.target);
+                }
+            })
+
+    }, imgOptions);
+
+    images.forEach(image => {
+        imgObserver.observe(observe)
+    })
