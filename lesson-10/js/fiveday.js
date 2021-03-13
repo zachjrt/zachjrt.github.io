e108d9c9e2aa86519d27f0a8cea9557429c4d680
zachjrt <tho19091@byui.edu>
2021-03-13 11:14:55 -0700

    commit

--- lesson-10/js/fiveday.js
+++ lesson-10/js/fiveday.js
@@ -12,13 +12,14 @@ fetch(apiURL_forecast)
 
   const weekday = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 
-   let day = 0;
+   let forecastday = 0;
   forecastData.forEach(forecast => {
     let x = new Date(forecast.dt_txt);
-   document.getElementById('temp'+(day +1)).textContent = Math.round(forecast.main.temp) + ' °F';
-   document.getElementById('img'+(day +1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
-   document.getElementById('img'+(day +1)).alt = forecast.weather[0].description
-  document.getElementById('day'+(day +1)).textContent = weekday[x.getDay()];
-  day++;	  
+   document.getElementById('temp'+(forecastday+1)).textContent = Math.round(forecast.main.temp) + ' °F';
+   document.getElementById('img'+(forecastday+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+   document.getElementById('img'+(forecastday+1)).alt = forecast.weather[0].description
+  document.getElementById('forecastday'+(forecastday+1)).textContent = weekday[x.getDay()];
+  forecastday++;	  
   });
 });
+
