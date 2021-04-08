0f8f40aecd1ee40448d579b2cad157dc58c77df6
zachjrt <tho19091@byui.edu>
2021-04-07 19:50:50 -0700

    commit

--- Final/js/businesspull.js
+++ Final/js/businesspull.js
@@ -18,19 +18,28 @@ fetch(requestURL)
         let picture = document.createElement('img');
         let address = document.createElement('p');
         let cityName = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let website = document.createElement('a');
 
         h4.textContent = commerce[i].name;
 
         
         address.textContent = 'Street Address: '+ commerce[i].streetAddress;
-        cityName.textContent = 'Place Born: ' + commerce[i].city;
+        cityName.textContent = '' + commerce[i].city + ', ' + commerce[i].state + ', ' + commerce[i].postalCode;
+        website.textContent = '' + commerce[i].mainAddress;
+        phoneNumber.textContent = '' + commerce[i].number;
 
+
+        website.setAttribute('href', commerce[i].mainAddress);
         picture.setAttribute('src', commerce[i].img);
-        picture.setAttribute('alt', commerce[i].name + ' ' +  'picture' + )
+        picture.setAttribute('alt', commerce[i].name + ' ' +  'picture')
+
 
         card.appendChild(h4);
-        card.appendChild(birthDate);
-        card.appendChild(birthPlace);
+        card.appendChild(address);
+        card.appendChild(cityName);
+        card.appendChild(phoneNumber);
+        card.appendChild(website);
         card.appendChild(picture);
 
 
